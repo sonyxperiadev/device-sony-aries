3fe3c3da48502ba47f3ec4f8c6f87783662852e8
krabappel2548 <krabappel2548@gmail.com>
2014-11-16 17:20:58 +0100

    add sensor_def_qcomdev.conf

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
     device/sony/aries/rootdir/logo.rle:root/logo.rle \
     device/sony/aries/rootdir/system/etc/BCM4354.hcd:system/etc/firmware/BCM43xx.hcd \
     device/sony/aries/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
+    device/sony/aries/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     device/sony/aries/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/sony/aries/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
 
