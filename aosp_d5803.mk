abdb52b8f89d36954de5498d912569796e693fc9
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2014-12-19 10:31:43 +0100

    bcmdhd: use sony fw for now
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product-if-exists, device/sony/shinano/device.mk)
 $(call inherit-product-if-exists, vendor/sony/aries/aries-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
+#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
 
 PRODUCT_COPY_FILES += \
     device/sony/aries/rootdir/logo.rle:root/logo.rle \
