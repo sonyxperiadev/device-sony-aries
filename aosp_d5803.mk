b666eb794bb630e1cb329ba78ba143783d0e2633
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-01-22 17:19:02 +0100

    bcmdhd: move bcmdhd.cal to standard location
    move bcmdhd.cal to /system/etc/wifi floowing AOSP layout
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -25,7 +25,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     device/sony/aries/rootdir/logo.rle:root/logo.rle \
     device/sony/aries/rootdir/system/etc/BCM4354.hcd:system/etc/firmware/BCM43xx.hcd \
-    device/sony/aries/rootdir/system/etc/bcmdhd.cal:system/etc/firmware/wlan/bcmdhd/bcmdhd.cal \
+    device/sony/aries/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/sony/aries/rootdir/system/etc/sensors_calib.conf:system/etc/sensors_calib.conf \
     device/sony/aries/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     device/sony/aries/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
