bbcd76c5e65caa3d6e9f1138ffa783182abeda2d
Adam Farden <adam@farden.cz>
2015-02-10 20:06:34 +0100

    Define the device's kernel defconfig
    for inline kernel building we have to define
    kernel config for each device

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_KERNEL_CONFIG := aosp_shinano_aries_defconfig
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
