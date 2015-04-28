eedfa65c1ac659a0c41fc55428405aa6d4b46586
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-28 15:49:38 +0200

    add optional gms suite
    if optional gms repo is found gms is added to build
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/sony/aries/aries-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 $(call inherit-product-if-exists, prebuilts/chromium/webview_prebuilt.mk)
+$(call inherit-product-if-exists, vendor/google/products/gms.mk)
 
 PRODUCT_COPY_FILES += \
     device/sony/aries/rootdir/system/etc/BCM4339.hcd:system/etc/firmware/BCM43xx.hcd \
