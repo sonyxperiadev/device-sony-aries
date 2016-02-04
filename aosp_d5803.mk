2cffec6d3bf35ede279640d77be4d530e542b995
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-04 23:04:35 +0100

    move common bootimg to common
    move common KERNEL_CMDLINE and init.*.rc to common git
    for easy maintenance
    
    move device HALs to device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5803.mk
+++ aosp_d5803.mk
@@ -68,9 +68,23 @@ PRODUCT_COPY_FILES += \
     device/sony/aries/rootdir/system/etc/tfa98xx/VoiceCallEarpice_top.preset:/system/etc/tfa98xx/VoiceCallEarpice_top.preset \
     device/sony/aries/rootdir/system/etc/tfa98xx/VoiceCallEarpice_top.eq:/system/etc/tfa98xx/VoiceCallEarpice_top.eq
 
+# Device Init
+PRODUCT_PACKAGES += \
+    init.recovery.aries \
+    init.aries \
+    ueventd.aries
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.aries
+
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.aries
+
 # NFC config
-PRODUCT_PACKAGES += nfc_nci.aries
-ADDITIONAL_DEFAULT_PROPERTIES += ro.hardware.nfc_nci=aries
+PRODUCT_PACKAGES += \
+    nfc_nci.aries
 
 PRODUCT_NAME := aosp_d5803
 PRODUCT_DEVICE := aries
